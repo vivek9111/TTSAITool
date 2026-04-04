c6d577e7cdcd60a9cfdb77003d5a6f117c4d4698
vivek9111 <vs96871@gmail.com>
2026-04-04 06:32:31 +0530

    Update response to FileResponse

--- app/services/VoiceCloningService.py
+++ app/services/VoiceCloningService.py
@@ -3,20 +3,24 @@ from app.services.VoiceService import VoiceService
 from app.utils.TextNormalizerIndic import IndicXTTSPreProcessor
 
 class VoiceCloningService:
+    
+    def __init__(self):
+        self.voiceService = VoiceService()
+        self.indicXTTSPreProcessor = IndicXTTSPreProcessor()
 
     def cloneVoiceFromId(self, voiceId: str, text: str, language: str = "hi"):
+        
+        if not text:
+            raise ValueError("Input text is empty")
 
         # get stored reference wav file for the given voiceId
-        speaker_wav = VoiceService.getVoiceFile(voiceId)
+        speaker_wav = self.voiceService.getVoiceFile(voiceId)
         
         if not speaker_wav:
             raise ValueError("Speaker wav file not found for voiceId: " + voiceId)
-
-        # normalize input text using Indic NLP library for better XTTS performance
-        cleanText = IndicXTTSPreProcessor().clean_for_xtts(text, language)
         
-        if not cleanText:
-            raise ValueError("Input text is empty")
+        # normalize input text using Indic NLP library for better XTTS performance
+        cleanText = self.indicXTTSPreProcessor.clean_for_xtts(text, language)
 
         # get globally loaded XTTS model
         xtts = ModelRegistry.getXttsModel()
